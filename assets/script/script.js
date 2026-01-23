1511fe0b1b4629c2573d60e6560db17970677589
unknown <gear.misha.mx@gmail.com>
2026-01-23 22:32:26 +0200

    feat:add new page about us;add new page branding;change font-size for mobile and tablet

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,27 +1,36 @@
 const toTop = document.querySelector("#scrol-to-top")
 
-toTop.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
+if (toTop) {
+    toTop.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    })
+}
+
 
 const send = document.querySelectorAll("#send")
 
-send.forEach(button => {
-    button.addEventListener("click", () => {
-        alert("Собщение отправлено")
+if (send) {
+    send.forEach(button => {
+        button.addEventListener("click", () => {
+            alert("Собщение отправлено")
+        })
     })
-})
+}
 
-const toDown = document.querySelector("#scroll-to-down")
 
-toDown.addEventListener("click", () => {
-    window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: "smooth"
+const toDown = document.querySelectorAll("#scroll-to-down")
+
+if (toDown) {
+    toDown.forEach(button => {
+        button.addEventListener("click", () => {
+            window.scrollTo({
+                top: document.body.scrollHeight,
+                behavior: "smooth"
+            })
+        })
     })
-})
-
+}
 
